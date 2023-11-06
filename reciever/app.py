c4b56cf41f79b6caec55f3c964f8c6aa29a41a71
Raqin Shikder <raqin2003@Gmail.com>
2023-11-06 15:50:57 -0800

    added kafka config to reciever service

--- reciever/app.py
+++ reciever/app.py
@@ -1,5 +1,5 @@
 import connexion
-from flask import jsonify,request
+from flask import jsonify, request
 from connexion import NoContent
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -10,14 +10,11 @@ import logging
 import logging.config
 import uuid
 import yaml
-import datetime
 import json
-import requests
-
+from pykafka import KafkaClient
 
 with open('reciever/app_conf.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
-
 
 with open('reciever/log_conf.yml', 'r') as f:
     log_config = yaml.safe_load(f.read())
@@ -26,36 +23,49 @@ with open('reciever/log_conf.yml', 'r') as f:
 logger = logging.getLogger('basicLogger')
 
 def report_order_status(body):
-    
     trace_id = uuid.uuid4() 
     body["trace_id"] = str(trace_id)
 
-    mysql_endpoint = app_config['eventstore1']['url']
-    headers = {"Content-Type": "application/json"}
-    response = requests.post(mysql_endpoint, json=body, headers=headers)
+    # Kafka Producer setup
+    client = KafkaClient(hosts=f"{app_config['events']['hostname']}:{app_config['events']['port']}")
+    topic = client.topics[str.encode(app_config['events']['topic'])]
+    producer = topic.get_sync_producer()
+
+    msg = {
+        "type": "order_status",
+        "datetime": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+        "payload": body
+    }
+    msg_str = json.dumps(msg)
+    producer.produce(msg_str.encode('utf-8'))
 
     logger.info(f"Received event report_order_status request with a trace id of {trace_id}")
-    logger.info(f"Returned event report_order_status response (Id: {trace_id}) with status {response.status_code}")
+    logger.info(f"Produced event report_order_status message with trace id {trace_id}")
     return NoContent, 201
 
 def reportETA(body):
-    trace_id = uuid.uuid4()  # Can use uuid, random, datetime
+    trace_id = uuid.uuid4()
     body["trace_id"] = str(trace_id)
 
-    mysql_endpoint = app_config['eventstore2']['url']
-    headers = {"Content-Type": "application/json"}
-    response = requests.post(mysql_endpoint, json=body, headers=headers)
+    # Kafka Producer setup
+    client = KafkaClient(hosts=f"{app_config['events']['hostname']}:{app_config['events']['port']}")
+    topic = client.topics[str.encode(app_config['events']['topic'])]
+    producer = topic.get_sync_producer()
+
+    msg = {
+        "type": "ETA",
+        "datetime": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+        "payload": body
+    }
+    msg_str = json.dumps(msg)
+    producer.produce(msg_str.encode('utf-8'))
 
     logger.info(f"Received event reportETA request with a trace id of {trace_id}")
-    logger.info(f"Returned event reportETA response (Id: {trace_id}) with status {response.status_code}")
+    logger.info(f"Produced event reportETA message with trace id {trace_id}")
     return NoContent, 201
 
-
-app = connexion.FlaskApp(__name__,specification_dir='')
+app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yml")
-
-
 
 if __name__ == "__main__":
     app.run(port=8080)
-
