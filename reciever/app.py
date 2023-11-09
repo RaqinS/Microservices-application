33c6f0d1540636b20be803a0f9872db3db1c872f
raqin shikder <96571092+RaqinS@users.noreply.github.com>
2023-11-08 23:48:08 -0800

    Update app.py
    .

--- reciever/app.py
+++ reciever/app.py
@@ -13,10 +13,10 @@ import yaml
 import json
 from pykafka import KafkaClient
 
-with open('reciever/app_conf.yml', 'r') as f:
+with open('app_conf.yml', 'r') as f:
     app_config = yaml.safe_load(f.read())
 
-with open('reciever/log_conf.yml', 'r') as f:
+with open('log_conf.yml', 'r') as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
